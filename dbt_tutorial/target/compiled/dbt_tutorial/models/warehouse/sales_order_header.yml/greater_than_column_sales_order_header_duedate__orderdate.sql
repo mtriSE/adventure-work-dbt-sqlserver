399ad3d2a04a0b75d92695359089e1596c792ba6
mtriSE <mtri0162@gmail.com>
2024-08-27 21:16:26 +0700

    clean dbt project

--- dbt_tutorial/target/compiled/dbt_tutorial/models/warehouse/sales_order_header.yml/greater_than_column_sales_order_header_duedate__orderdate.sql
+++
@@ -1,7 +0,0 @@
-
-
-SELECT duedate, orderdate, count(1) as num_occurrences
-FROM "Adventureworks"."warehouse"."sales_order_header"
-WHERE duedate < orderdate
-GROUP BY 1, 2
-
--- dbt_tutorial/target/compiled/dbt_tutorial/tests/test_due_date_before_order_date.sql
+++
@@ -1,4 +0,0 @@
-SELECT duedate, orderdate, count(1) as occurences
-FROM "Adventureworks"."warehouse"."sales_order_header"
-WHERE duedate < orderdate
-GROUP BY duedate, orderdate
